21b3149d3c75059b89bde681081309ef8aa37b96
Volodia Dubanov <dubanovinfo@gmail.com>
2021-02-28 23:02:04 +0300

    new file:   Demo1.java 	new file:   Demo2.java 	new file:   Demo3.java 	new file:   Demo4.txt 	new file:   README.md

---
+++ Demo1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Demo1 {
+
+    public static void main(String[] args) {
+		
+    	Scanner scan = new Scanner(System.in);
+    	System.out.print("Введите натуральное число: ");
+
+    	// This method reads the number provided using keyboard
+    	int num = scan.nextInt();
+    	scan.close();
+        
+    	// Check if number is greater than 7
+    	if (num > 7) {
+    		System.out.println("Привет");
+    	}
+    }
+}
+
